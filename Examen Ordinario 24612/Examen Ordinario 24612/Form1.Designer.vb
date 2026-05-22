10d836c7ba0bd80a780769bc3fdd3996f149f08d
diego-espinosa-17 <diego.espinosa@iest.edu.mx>
2026-05-22 09:15:32 -0600

    Aplicación de Tienda

---
+++ Examen Ordinario 24612/Examen Ordinario 24612/Form1.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Restar = New System.Windows.Forms.Button()
+        Me.Suma = New System.Windows.Forms.Button()
+        Me.BotonSubtotal = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.subtotal = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.IVA = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.total = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(56, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(306, 33)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Aplicacion de Tienda"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(32, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Precio:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(32, 160)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Cantidad:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(196, 157)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(106, 26)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(116, 92)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(19, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "$"
+        '
+        'Restar
+        '
+        Me.Restar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Restar.Location = New System.Drawing.Point(124, 153)
+        Me.Restar.Name = "Restar"
+        Me.Restar.Size = New System.Drawing.Size(52, 34)
+        Me.Restar.TabIndex = 5
+        Me.Restar.Text = "-1"
+        Me.Restar.UseVisualStyleBackColor = True
+        '
+        'Suma
+        '
+        Me.Suma.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Suma.Location = New System.Drawing.Point(321, 153)
+        Me.Suma.Name = "Suma"
+        Me.Suma.Size = New System.Drawing.Size(54, 34)
+        Me.Suma.TabIndex = 7
+        Me.Suma.Text = "+1"
+        Me.Suma.UseVisualStyleBackColor = True
+        '
+        'BotonSubtotal
+        '
+        Me.BotonSubtotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BotonSubtotal.Location = New System.Drawing.Point(36, 224)
+        Me.BotonSubtotal.Name = "BotonSubtotal"
+        Me.BotonSubtotal.Size = New System.Drawing.Size(184, 37)
+        Me.BotonSubtotal.TabIndex = 8
+        Me.BotonSubtotal.Text = "Calcular Subtotal"
+        Me.BotonSubtotal.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(36, 288)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(184, 34)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Calcular IVA"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(36, 351)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(184, 34)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "Calcular Total"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(233, 232)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 20)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Subtotal:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(321, 232)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(19, 20)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "$"
+        '
+        'subtotal
+        '
+        Me.subtotal.AutoSize = True
+        Me.subtotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.subtotal.Location = New System.Drawing.Point(343, 232)
+        Me.subtotal.Name = "subtotal"
+        Me.subtotal.Size = New System.Drawing.Size(19, 20)
+        Me.subtotal.TabIndex = 13
+        Me.subtotal.Text = "0"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(233, 295)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(96, 20)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "IVA (16%):"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(335, 296)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(19, 20)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "$"
+        '
+        'IVA
+        '
+        Me.IVA.AutoSize = True
+        Me.IVA.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.IVA.Location = New System.Drawing.Point(356, 296)
+        Me.IVA.Name = "IVA"
+        Me.IVA.Size = New System.Drawing.Size(19, 20)
+        Me.IVA.TabIndex = 16
+        Me.IVA.Text = "0"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(233, 358)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(69, 20)
+        Me.Label11.TabIndex = 17
+        Me.Label11.Text = "TOTAL:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(308, 358)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(19, 20)
+        Me.Label12.TabIndex = 18
+        Me.Label12.Text = "$"
+        '
+        'total
+        '
+        Me.total.AutoSize = True
+        Me.total.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.total.Location = New System.Drawing.Point(331, 358)
+        Me.total.Name = "total"
+        Me.total.Size = New System.Drawing.Size(19, 20)
+        Me.total.TabIndex = 19
+        Me.total.Text = "0"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(141, 89)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(122, 26)
+        Me.TextBox1.TabIndex = 20
+        Me.TextBox1.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(467, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.total)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.IVA)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.subtotal)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.BotonSubtotal)
+        Me.Controls.Add(Me.Suma)
+        Me.Controls.Add(Me.Restar)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Restar As Button
+    Friend WithEvents Suma As Button
+    Friend WithEvents BotonSubtotal As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents subtotal As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents IVA As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents total As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Examen Ordinario 24612/Examen Ordinario 24612/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub BotonSubtotal_Click(sender As Object, e As EventArgs) Handles BotonSubtotal.Click
+        subtotal.Text = Val(TextBox2.Text) * Val(TextBox1.Text)
+    End Sub
+
+    Private Sub Restar_Click(sender As Object, e As EventArgs) Handles Restar.Click
+        If Val(TextBox2.Text) = 0 Then
+            TextBox2.Text = 0
+        ElseIf Val(TextBox2.Text) >= 0 Then
+            TextBox2.Text = Val(TextBox2) - 1
+        End If
+    End Sub
+
+    Private Sub Suma_Click(sender As Object, e As EventArgs) Handles Suma.Click
+        TextBox2.Text = Val(TextBox2) + 1
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        IVA.Text = Val(subtotal.Text) * 0.16
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        total.Text = Val(subtotal.Text) + Val(IVA.Text)
+    End Sub
+End Class
